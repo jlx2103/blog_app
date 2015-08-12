fb7c7c2d108257d119e1395304b1a7ed8b39688f
JLXdev <jlxdev@Josues-MacBook-Pro.local>
2015-08-12 08:31:16 -0400

    thrid commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   resources :le_blogs
+
+  root "le_blogs#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
